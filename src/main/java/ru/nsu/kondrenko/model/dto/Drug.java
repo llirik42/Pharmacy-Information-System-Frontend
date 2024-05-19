8108553dbfdeee7f6a138d187867e0c3db02f787
Kirill Kondrenko <kondrenkokp@yandex.ru>
2024-05-19 11:45:38 +0700

    Added id to Drug and DrugType

--- src/main/java/ru/nsu/kondrenko/model/dto/Drug.java
+++ src/main/java/ru/nsu/kondrenko/model/dto/Drug.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Drug {
+    private int id;
+
     private String name;
 
     private int cost;
--- src/main/java/ru/nsu/kondrenko/model/dto/DrugType.java
+++ src/main/java/ru/nsu/kondrenko/model/dto/DrugType.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DrugType {
+    private int id;
+
     private String name;
 
     private boolean isCookable;
