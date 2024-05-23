2357af2566bb4406c62d3757bc70ab272cc07f50
Kirill Kondrenko <kondrenkokp@yandex.ru>
2024-05-23 16:52:49 +0700

    Removed unused imports

--- src/main/java/ru/nsu/kondrenko/model/dto/StoredDrug.java
+++ src/main/java/ru/nsu/kondrenko/model/dto/StoredDrug.java
@@ -1,6 +1,5 @@
 package ru.nsu.kondrenko.model.dto;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
