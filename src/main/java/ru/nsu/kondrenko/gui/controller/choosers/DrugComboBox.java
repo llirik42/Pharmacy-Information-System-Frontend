de562b2ae3556eefe5b27bed5d981f9f404b18d6
Kirill Kondrenko <kondrenkokp@yandex.ru>
2024-05-23 18:17:05 +0700

    Implemented DrugComboBox

--- src/main/java/ru/nsu/kondrenko/gui/controller/choosers/DrugComboBox.java
+++ src/main/java/ru/nsu/kondrenko/gui/controller/choosers/DrugComboBox.java
@@ -3,13 +3,56 @@ package ru.nsu.kondrenko.gui.controller.choosers;
 import ru.nsu.kondrenko.model.dto.Drug;
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DrugComboBox extends JComboBox<String> {
-    public void setDrugList() {
-        setModel(new DefaultComboBoxModel<>(new String[]{"a", "b", "c", "d"}));
+    private List<Drug> drugs;
+
+    private Drug prevSelectedDrug = null;
+
+    public void setDrugList(List<Drug> drugs) {
+        this.drugs = new ArrayList<>(drugs);
+        this.drugs.sort((d1, d2) -> d1.getName().compareToIgnoreCase(d2.getName()));
+
+        changeModel();
+        selectPreviousDrug();
     }
 
     public Drug getSelectedDrug() {
-        return new Drug();
+        final int selectedIndex = getSelectedIndex();
+        prevSelectedDrug = selectedIndex == 0 ? null : drugs.get(getDrugIndexByItemIndex(selectedIndex));
+        return prevSelectedDrug;
+    }
+
+    private void changeModel() {
+        final String[] drugNames = new String[drugs.size() + 1];
+
+        drugNames[0] = "";
+        for (int i = 0; i < drugs.size(); i++) {
+            final Drug drug = this.drugs.get(i);
+            final int itemIndex = getItemIndexByDrugIndex(i);
+            drugNames[itemIndex] = drug.getName();
+        }
+
+        setModel(new DefaultComboBoxModel<>(drugNames));
+    }
+
+    private void selectPreviousDrug() {
+        if (prevSelectedDrug != null) {
+            final int prevSelectedDrugIndex = this.drugs.indexOf(prevSelectedDrug);
+
+            if (prevSelectedDrugIndex != -1) {
+                setSelectedIndex(getItemIndexByDrugIndex(prevSelectedDrugIndex));
+            }
+        }
+    }
+
+    private int getItemIndexByDrugIndex(int drugIndex) {
+        return drugIndex + 1;
+    }
+
+    private int getDrugIndexByItemIndex(int itemIndex) {
+        return itemIndex - 1;
     }
 }
